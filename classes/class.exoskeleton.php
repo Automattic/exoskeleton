083db1d9ecf95aff6115b88ca775adc7d8a96031
Matt Perry <mattperry@Matts-Air-2.lan>
2017-03-20 14:04:38 -0700

    Add inline docs, remove one un-needed param
    PHPDocs for all.  Also, remove a particular arg that wasn’t needed.

--- classes/class.exoskeleton.php
+++ classes/class.exoskeleton.php
@@ -1,19 +1,36 @@
 <?php
 
+/**
+ * The main Exoskeleton Class
+ */
 class Exoskeleton {
 
+	/**
+	 * @var Exoskeleton
+	 */
 	private static $instance;
 
 	const LOCK_PREFIX = 'exoskeleton_lock_';
 	const COUNTER_PREFIX = 'exoskeleton_counter_';
 
+	/**
+	 * @var Array
+	 */
 	public $rules = [];
 
+	/**
+	 * Constructor
+	 * @return null
+	 */
 	private function __construct() {
 		add_action( 'rest_api_init', [ $this, 'process_custom_routes' ], 999, 1 );
 		add_filter( 'rest_pre_dispatch', [ $this, 'shields_up' ], 5, 3 );
 	}
 
+	/**
+	 * Gets or makes an instance
+	 * @return Exoskeleton
+	 */
 	public static function get_instance() {
 		if ( !self::$instance ) {
 			self::$instance = new Exoskeleton;
@@ -21,6 +38,11 @@ class Exoskeleton {
 		return self::$instance;
 	}
 
+	/**
+	 * Add a rule after validating it
+	 * @param Array $args 
+	 * @return Bool whether or not the rule was added
+	 */
 	public function add_rule( $args ) {
 		$defaults = [
 			'method' => 'any',
@@ -41,18 +63,32 @@ class Exoskeleton {
 		}
 	}
 
+	/**
+	 * Makes a key for the rule based on its method and argyments
+	 * @param Array $rule 
+	 * @return String the generated key
+	 */
 	private function generate_rule_key( $rule ) {
 		$method = $rule[ 'method' ];
 		unset( $rule[ 'method' ] );
 		return md5( serialize( $rule ) ) . '_' . $method;
 	}
 
+	/**
+	 * Turn a _method key into an _any key
+	 * @param String $key 
+	 * @return String the key with a _any suffix
+	 */
 	private function anyize_rule_key( $key ) {
 		$parts = explode( '_', $key );
 		return $parts[0] . '_any';
 	}
 
-	//@todo make this
+	/**
+	 * Make sure a rule is valid.  Valid rules are those with a valid method or "any", a positive numeric argument for lockout, window and limit and a boolean argument for treat_head_like_get.  We don't validate routes.
+	 * @param Array $args 
+	 * @return Bool whether the rule is valid
+	 */
 	public function validate_rule( $args ) {
 
 		//every rule must have
@@ -102,6 +138,11 @@ class Exoskeleton {
 		return (bool) $valid;
 	}
 
+	/**
+	 * Custom (non-built-in) routes receive their rules when they are defined.  This callback sniffs out rules from all the server's endpoints and adds them.
+	 * @param WP_REST_Server $server 
+	 * @return null
+	 */
 	public function process_custom_routes( $server ) {
 		foreach( $server->get_routes() as $path => $endpoints ) {
 			foreach ( $endpoints as $endpoint ) {
@@ -122,6 +163,13 @@ class Exoskeleton {
 		}
 	}
 
+	/**
+	 * Decide whether a given WP_REST_Request is subject to an Exoskeleton Rule.  If so, it invoke the limiter.
+	 * @param WP_REST_Response $response 
+	 * @param WP_REST_Server $handler 
+	 * @param WP_REST_Request $request 
+	 * @return WP_REST_Response | null
+	 */
 	public function shields_up( $response, $handler, $request ) {
 
 		// if there's already a nonempty response, someone else got here first, so bail
@@ -138,13 +186,19 @@ class Exoskeleton {
 			if ( 1 === $match ) {
 				if ( $this->match_rule_method( $method, $rule ) ) {
 					$matched_rule = [ $key => $rule ];
-					$this->maybe_back_off( $matched_rule, $request );	
+					$this->maybe_back_off( $matched_rule );	
 				}
 			}
 		}
 		return $response;
 	}
 
+	/**
+	 * For a given rule, does the method match?
+	 * @param String $method 
+	 * @param Array $rule 
+	 * @return Bool whether or not the method applies to the rule
+	 */
 	private function match_rule_method( $method, $rule ) {
 
 		//by default we meter HEAD requests as though they were GETs  rules may override this for particular routes
@@ -158,8 +212,12 @@ class Exoskeleton {
 		);
 	}
 
-
-	private function maybe_back_off( $matched_rule, $request ) {
+	/**
+	 * Decide whether to tell the request to back off based on the current state of the applicable rule's lock.  Also, increment the rule's counter, and set a lock if needed.
+	 * @param Array $matched_rule 
+	 * @return null
+	 */
+	private function maybe_back_off( $matched_rule ) {
 		
 		$rule_id = array_keys( $matched_rule )[0];
 		$lock = $this->get_lock( $rule_id );
@@ -177,6 +235,11 @@ class Exoskeleton {
 		}
 	}
 
+	/**
+	 * Check for a lock on a given rule or it's universal version
+	 * @param String $rule_id 
+	 * @return Array | Bool  the lock, or false if no lock is found
+	 */
 	private function get_lock( $rule_id ) {
 		$lock = self::LOCK_PREFIX . $rule_id;
 		//we might need to check for universal locks and not just the method requested
@@ -195,13 +258,25 @@ class Exoskeleton {
 		return $found_lock;
 	}
 
+	/**
+	 * Sets a lock (transient) lasting lockout seconds for a matched rule_id
+	 * @param String $rule_id 
+	 * @param Array $matched_rule 
+	 * @return Bool whether or not the lock was successfully set
+	 */
 	private function set_lock( $rule_id, $matched_rule ) {
 		$lock = self::LOCK_PREFIX . $rule_id;
 		$lock_data = [ 'lockout' => $matched_rule[ $rule_id ]['lockout' ], 'lock_set' => time() ];
 		return set_transient( $lock, $lock_data, $matched_rule[ $rule_id ]['lockout' ] );
 	}
 
-
+	/**
+	 * Terminate the request after sending a 429 and Retry-After header
+	 * @param String $rule_id 
+	 * @param Array $matched_rule 
+	 * @param Array $lock 
+	 * @return null
+	 */
 	private function back_off_please( $rule_id, $matched_rule, $lock ) {
 
 		$lockout = $matched_rule[ $rule_id ][ 'lockout' ];
@@ -211,6 +286,12 @@ class Exoskeleton {
 		die();
 	}
 
+	/**
+	 * Increment the counter for requests on the matched endpoint.
+	 * @param String $rule_id 
+	 * @param Array $matched_rule 
+	 * @return Int the value of the counter after increment
+	 */
 	private function increment_counter( $rule_id, $matched_rule ) {
 
 		$counter_id = self::COUNTER_PREFIX . $rule_id;
--- exoskeleton.php
+++ exoskeleton.php
@@ -10,11 +10,21 @@
 
 require_once( plugin_dir_path( __FILE__ ) . 'classes/class.exoskeleton.php' );
 
+/**
+ * Add a rule to an existing endpoint
+ * @param Array $args 
+ * @return Bool whether or not the rule was added
+ */
 function exoskeleton_add_rule( $args ) {
 	$e = Exoskeleton::get_instance();
 	return $e->add_rule( $args );
 }
 
+/**
+ * Add many rules at once
+ * @param Array $rules  an array of rules arrays
+ * @return null
+ */
 function exoskeleton_add_rules( $rules ) {
 	foreach ( $rules as $rule ) {
 		exoskeleton_add_rule( $rule );
